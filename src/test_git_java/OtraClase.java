f1b4cb726bc23234f905568baf260f4fca18e0d0
Alvaro <aduyaguari@gmail.com>
2024-10-30 09:04:44 -0500

    segundo commit

---
+++ src/test_git_java/OtraClase.java
@@ -0,0 +1,10 @@
+package test_git_java;
+
+public class OtraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo 2");
+	}
+
+}
